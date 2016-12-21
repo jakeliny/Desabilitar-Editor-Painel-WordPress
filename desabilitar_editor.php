7aaedfc69cfd6109625c9bea2ed71f2ec5c9d36a
jakeliny <jakeliny.gr@gmail.com>
2016-12-21 14:28:54 -0200

    corrigindo bug que conflitava com plugin de ocultar barra admin

--- desabilitar_editor.php
+++ desabilitar_editor.php
@@ -3,8 +3,8 @@
 /*
 Plugin Name: Desabilitar Editor do Painel
 Plugin URI: https://github.com/jakeliny/desabilitar_editor
-Description: Desabilitar o editor de código no painel admin
-Version:    1.0.0
+Description: Desabilitar o editor de código no painel admin do WordPress
+Version:    1.1.2
 Author:      Jakeliny
 Author URI: https://github.com/jakeliny
 License:    GPL2
@@ -13,12 +13,7 @@ Text Domain: wporg
 Domain Path: /languages
 */
 
-/*Escondendo a barra administrativa*/
-function my_function_admin_bar(){
-    return false;
-}
-
-class Ocultar_Barra_Admin{
+class DisableEditor{
 
   static function initial(){
     define('DISALLOW_FILE_EDIT', true);
@@ -26,9 +21,9 @@ class Ocultar_Barra_Admin{
 
 
 
-  static function ocultar_barra(){
+  static function disable_editor(){
     return false;
   }
 }
 
-Ocultar_Barra_Admin::initial();
+DisableEditor::initial();
